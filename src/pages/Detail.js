cc9b4984bb8900cee35ab502c3c4cc18af75ce65
hheeju98 <hheeju98@naver.com>
2023-05-22 23:46:40 +0900

    글 삭제 구현 완료

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function Detail() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [post, setPost] = useState(null);
 
   useEffect(() => {
@@ -11,6 +11,20 @@ export default function Detail() {
     const selectedPost = savedPosts.find((post) => post.id === id);
     setPost(selectedPost);
   }, [id]);
+
+  const handleDeletePost = () => {
+    const savedPosts = JSON.parse(localStorage.getItem("posts")) || [];
+    const updatedPosts = savedPosts.filter((post) => post.id !== id);
+    localStorage.setItem("posts", JSON.stringify(updatedPosts));
+    navigate("/");
+  };
+
+  const confirmDelete = () => {
+    const shouldDelete = window.confirm("이 일기를 정말 삭제하시겠습니까?");
+    if (shouldDelete) {
+      handleDeletePost();
+    }
+  };
 
   if (!post) {
     return <div>글을 불러오는 중입니다...</div>;
@@ -21,6 +35,7 @@ export default function Detail() {
       <h1>{post.title}</h1>
       <p>{post.content}</p>
       <p>날짜: {post.date}</p>
+      <button onClick={confirmDelete}>삭제</button>
     </div>
   );
 }
