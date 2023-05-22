8d091c2369af5bad919bc0ea0332209948b198fe
hheeju98 <hheeju98@naver.com>
2023-05-22 23:28:37 +0900

    글 목록 페이징 완료

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,9 @@ import { onUserStateChange } from "../api/firebase";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 10;
 
   useEffect(() => {
     onUserStateChange(setUser);
@@ -15,10 +17,22 @@ export default function Home() {
     setPosts(savedPosts);
   }, []);
 
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // 현재 페이지에 해당하는 글 목록 필터링
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // 총 페이지 수 계산
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+
   return (
     <div>
       {user &&
-        posts.map((post) => (
+        currentPosts.map((post) => (
           <Link key={post.id} to={`/detail/${post.id}`}>
             <div>{post.title.slice(0, 20)}</div>
           </Link>
@@ -29,6 +43,20 @@ export default function Home() {
           <button>글쓰기</button>
         </Link>
       )}
+
+      <div>
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+          (pageNumber) => (
+            <button
+              key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
+              disabled={pageNumber === currentPage}
+            >
+              {pageNumber}
+            </button>
+          )
+        )}
+      </div>
     </div>
   );
 }
