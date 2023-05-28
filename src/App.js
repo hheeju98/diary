b33a8328127a123b755adede82bed810fe5497a4
hheeju98 <hheeju98@naver.com>
2023-05-28 18:18:13 +0900

    로그인 페이지 Navbar중복 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 export default function App() {
   return (
     <div>
-      <Navbar />
       <Login />
     </div>
   );
