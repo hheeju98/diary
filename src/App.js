99468982682977795248c1cf65d66bd32fef0bff
hheeju98 <hheeju98@naver.com>
2023-05-28 18:49:17 +0900

    호스팅 에러 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 export default function App() {
   return (
     <div>
+      <Navbar />
       <Login />
     </div>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
 import { onUserStateChange } from "../api/firebase";
@@ -23,7 +22,6 @@ export default function Login() {
   }
   return (
     <div className="greeting-box">
-      <Navbar />
       <div className="greeting">
         <span className="greeting-text">
           오늘의 일기에 오신 것을 환영합니다.
